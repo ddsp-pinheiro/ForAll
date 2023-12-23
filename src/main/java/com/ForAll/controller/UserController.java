4d491c8dfb0106d6999f92e01287a3cf65b2b904
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2023-12-23 20:42:15 -0300

    Feature: Create new branch develop, class UserController and method postUser

---
+++ src/main/java/com/ForAll/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.ForAll.controller;
+
+import com.ForAll.model.UserModel;
+import com.ForAll.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public ResponseEntity<UserModel> postUser(@RequestBody UserModel user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.addUser(user));
+    }
+
+}
