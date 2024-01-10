afd8510887ecdfbd1b7722ef46208315761b418c
uPadrin <bryan.vieira2013@gmail.com>
2024-01-09 21:12:55 -0300

    Feat: creation of getter and setter methods in userModel

--- src/main/java/com/ForAll/model/UserModel.java
+++ src/main/java/com/ForAll/model/UserModel.java
@@ -28,4 +28,44 @@ public class UserModel {
     private String ddd;
     @Column(name = "num_tell", length = 9)
     private String tell;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getTell() {
+        return tell;
+    }
+
+    public void setTell(String tell) {
+        this.tell = tell;
+    }
 }
